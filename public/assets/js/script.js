0667b341c8edd5718108cc4dcbdfe7bbb4c57fdf
Corentin <corentin@seventeeninfo.fr>
2023-12-18 12:39:26 +0100

    Appuyer sur espace pour commencer

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -23,7 +23,8 @@ window.onload = function (){
         document.body.appendChild(canvas);
         //Context
         ctx = canvas.getContext('2d');
-        launch();
+        showStart();
+        // launch();
     }
 
     function launch(){
@@ -74,6 +75,22 @@ window.onload = function (){
         ctx.restore();
     }
 
+    function showStart(){
+        ctx.save();
+        ctx.font = 'bold 70px sans-serif';
+        ctx.fillStyle = 'black';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.strokeStyle = 'white';
+        ctx.lineWidth = '5';
+        ctx.strokeText('Jeu du serpent', centreX, centreY - 180);
+        ctx.fillText('Jeu du serpent', centreX, centreY - 180);
+        ctx.font = 'bold 30px sans-serif';
+        ctx.strokeText('Appuyer sur espace pour jouer', centreX, centreY - 120);
+        ctx.fillText('Appuyer sur espace pour jouer', centreX, centreY - 120);
+        ctx.restore();
+    }
+
     function drawScore() {
         ctx.save();
         ctx.font = 'bold 200px sans-serif';
@@ -101,7 +118,7 @@ window.onload = function (){
         this.ateApple = false;
         this.draw = function (){
             ctx.save();
-            ctx.fillStyle = "red";
+            ctx.fillStyle = "green";
             for(let i = 0; i < body.length; i++){
                 drawBlock(ctx, this.body[i]);
             }
@@ -187,7 +204,7 @@ window.onload = function (){
             const x = this.position[0] * blockSize + radius;
             const y = this.position[1] * blockSize + radius;
             ctx.save();
-            ctx.fillStyle = "#33cc33";
+            ctx.fillStyle = "red";
             ctx.beginPath();
             ctx.arc(x,y, radius, 0, Math.PI*2, true);
             ctx.fill();
